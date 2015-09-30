11a8b997884f818fef3210c94885c5ae8915d4c4
Kristopher Stoupe <kstoupe@yahoo.com>
2015-09-30 12:51:07 +1000

    [Added Menus]
    The game now uses a menu to navigate into the game. The original network manager HUD has been hidden, not removed just in case we need it later.
    Every UI element has also been anchored so screen resolution shouldn't affect the UI now.
    The UI will be improved upon as the game gets more developed and the options needed in the game are given more details.

---
+++ KaijuRising/Assets/Scripts/Menus/MenuCommands.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuCommands : MonoBehaviour {
+
+	public GameObject menuMain;
+	public GameObject menuStart;
+	public GameObject menuOptions;
+//	public GameObject menuMultiplayer;
+
+	void Start () 
+	{
+		hideAllMenus();
+		menuMain.SetActive(true);
+	}
+
+	private void hideAllMenus()
+	{
+		menuMain.SetActive(false);
+		menuStart.SetActive(false);
+		menuOptions.SetActive(false);
+//		menuMultiplayer.SetActive(false);
+	}
+
+	public void showStart()
+	{
+		hideAllMenus();
+		menuStart.SetActive(true);
+	}
+
+	public void hideStart()
+	{
+		hideAllMenus();
+		menuMain.SetActive(true);
+	}
+
+	public void showOptions()
+	{
+		hideAllMenus();
+		menuOptions.SetActive(true);
+	}
+
+	public void hideOptions()
+	{
+		hideAllMenus();
+		menuMain.SetActive(true);
+	}
+
+	public void showMultiplayer()
+	{
+		hideAllMenus();
+//		menuMultiplayer.SetActive(true);
+	}
+
+	public void hideMultiplayer()
+	{
+		hideAllMenus();
+		menuStart.SetActive(true);
+	}
+
+	public void quit()
+	{
+		Application.Quit();
+	}
+
+}
