cacccf8a126bf10c5b7a189fc65704e5e09c5e44
JordanDDisch <jordanddisch@gmail.com>
2015-10-13 12:37:49 +1100

    added comments to InstantiateSound script for reference

--- KaijuRising/Assets/Scripts/Sound/InstanciateSound.cs
+++ KaijuRising/Assets/Scripts/Sound/InstanciateSound.cs
@@ -13,18 +13,27 @@ public class InstanciateSound :  NetworkBehaviour {
 
 	public GameObject soundObject;
 	public sounds sounds;
+	public string soundFolderPath = "Sounds"; // references the path to the sounds folder in Resources folder
 
 	[ClientRpc]
 	public void RpcInstantiateOnClient(string sound) 
 	{
-		AudioClip theSound = Resources.Load("SoundWalk") as AudioClip;
+		// Have to send audio clip as a string param. Client RPC doesn't allow for certain param types, only string, int, float, bool.
+
+		// Find the audio clip
+		AudioClip theSound = Resources.Load(soundFolderPath + "/SoundWalk") as AudioClip;
+
+		// Instantiate sound on clients and self
 		GameObject instance = (GameObject)Instantiate(soundObject, transform.position, transform.rotation);
+
+		// Play the specified sound clip
 		instance.GetComponent<AudioSource> ().PlayOneShot(theSound);
 	}
 
 	[Command]
 	public void CmdInstantiateOnServer(string sound) 
 	{
+		// have to call RPC function from Command(server) if the being called from client, cannot run RPC functions from client.
 		RpcInstantiateOnClient (sound);
 	}
 }
