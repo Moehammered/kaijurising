dc28666bfd1d063b5d317538d1e158d04484a210
Billy Mihalarias <bmihalarias@gmail.com>
2015-10-28 22:40:25 +1100

    fixed kaiju Select screen and pink error

---
+++ KaijuRising/Assets/Scripts/UI/KaijuHighlight.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KaijuHighlight : MonoBehaviour
+{
+	private GameObject _selectedKaiju;
+	
+	public GameObject selectedKaiju
+	{
+		get
+		{
+			return _selectedKaiju;
+		}
+		set
+		{
+			dimLightSelected();
+			_selectedKaiju = value;
+			lightSelected();
+		}
+	}
+
+
+	public string lightLayerName;
+
+	public void hilightKaiju(GameObject kaiju)
+	{
+		//Debug.Log(kaiju.name);
+		selectedKaiju = kaiju;
+		//.play
+	}
+
+	void dimLightSelected()
+	{
+		if(selectedKaiju == null)
+			return;
+
+		Renderer[] childRends = selectedKaiju.GetComponentsInChildren<Renderer>();
+		foreach(Renderer r in childRends)
+		{
+			r.gameObject.layer = LayerMask.NameToLayer("Default");
+		}
+		selectedKaiju.GetComponent<Animator>().SetBool("KaijuClicked",false);
+	}
+
+	void lightSelected()
+	{
+		if(_selectedKaiju == null)
+			return;
+
+		Renderer[] childRends = selectedKaiju.GetComponentsInChildren<Renderer>();
+		foreach(Renderer r in childRends)
+		{
+			r.gameObject.layer =  LayerMask.NameToLayer(lightLayerName);
+		}
+		selectedKaiju.GetComponent<Animator>().SetBool("KaijuClicked",true);
+	}
+}
